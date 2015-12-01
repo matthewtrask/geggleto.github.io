cb17eb0f7faa12ee42c5ccfc26404384327a0ff2
Glenn Eggleton <geggleto@gmail.com>
2015-12-01 11:31:39 -0500

    Reenable twig caching

--- public/index.php
+++ public/index.php
@@ -8,7 +8,7 @@ $container['settings']['displayErrorDetails'] = true;
 
 $container['view'] = function ($c) {
     $view = new \Slim\Views\Twig('../templates', [
-        'cache' => false //'../cache'
+        'cache' => /*false*/ '../cache'
     ]);
 
     // Instantiate and add Slim specific extension
