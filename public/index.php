b8e280c90c536704c47010e8d029d354f9b7ed2c
Glenn Eggleton <geggleto@gmail.com>
2015-12-01 11:28:01 -0500

    Update

---
+++ public/index.php
@@ -0,0 +1,35 @@
+<?php
+require '../vendor/autoload.php';
+
+$app = new Slim\App();
+
+$container = $app->getContainer();
+$container['settings']['displayErrorDetails'] = true;
+
+$container['view'] = function ($c) {
+    $view = new \Slim\Views\Twig('../templates', [
+        'cache' => false //'../cache'
+    ]);
+
+    // Instantiate and add Slim specific extension
+    $view->addExtension(new Slim\Views\TwigExtension(
+        $c['router'],
+        $c['request']->getUri()
+    ));
+
+    return $view;
+};
+
+$app->get('/', function ($request, $response, $args) {
+    return $this->view->render($response, "index.twig", ["title" => "Welcome!"]);
+});
+
+$app->get('/articles/slim-intro', function ($request, $response, $args) {
+    return $this->view->render($response, "slim-intro.twig", ["title" => "Slim Introduction"]);
+});
+
+$app->get('/articles/php-app-architecture', function ($request, $response, $args) {
+    return $this->view->render($response, "php-app-architecture.twig", ["title" => "PHP App Architecture"]);
+});
+
+$app->run();
